ef2be1ce6de1adb5aef845251eb3e68b6147b797
ImNoOne-1999 <konasr@rknec.edu>
2020-10-29 15:32:49 +0530

    status code updTED

--- app3.py
+++ app3.py
@@ -1,30 +1,33 @@
 import pandas as pd
-import json,requests
+import json,requests,math
 from datetime import datetime
 
 now = datetime.now()
 
 current_time = now.strftime("%H:%M:%S")
 current_time = current_time.split(":")
-print(current_time)
+# print(current_time)
 
 col_list = ["Name", "MacID"]
 df = pd.read_csv("f.csv", usecols=col_list)
-response = requests.get('http://localhost:5000/api/devicefetcher')
+response = requests.get('http://iotrest.herokuapp.com/api/devicefetcher')
 # print(response.json())
 macid = ''
 for i in response.json():
     loginTime = i["loginTime"].split(":")
-    #print(loginTime) 
-    if (int(loginTime[0]) == int(current_time[0])) and (int(current_time[1]) - int(loginTime[1] ) >= int(current_time[1])):
+    # print(loginTime) 
+    totalLogTime = int(loginTime[0])*3600 + int(loginTime[1])*60 + int(loginTime[2])
+    totalCurrentTime = int(current_time[0])*3600 + int(current_time[1])*60 + int(current_time[2])
+    if abs(totalCurrentTime-totalLogTime) <= 600:
         macid = i["macId"]
 
 count = 0
 for j in df.MacID:
     if j == macid:
-        print("proceed")
-        count += 1
+        print("proceed"+macid)
         break
+    else:
+        count += 1
 
-if count == len(response.json()):
+if count == len(df):
     print("Abort Process")
